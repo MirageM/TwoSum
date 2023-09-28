2cb6d191526dff1868661894322ff7c6ffa936f1
MirageM <imiragesz@gmail.com>
2023-09-27 23:35:06 -0400

    nMap.containsKey(complement)

---
+++ TwoSumRedo2.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumRedo2{
+	public int[] twoSum(int[] nums, int target){
+		Map<Integer, Integer> nMap = new HashMap<>();
+		for(int i = 0; i < nums.length; i++){
+			nMap.put(nums[i], i);
+		}
+		for(int i = 0; i < nums.length; i++){
+			int complement = target - nums[i];
+			if(nMap.containsKey(complement) && nMap.get(complement) != i){
+				return new int[]{i, nMap.get(complement)};
+			}
+		}
+		return new int[]{};
+	}
+}
