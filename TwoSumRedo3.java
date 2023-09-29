fd45c69df5eac6daa00a82a15c0637d1d697a245
MirageM <imiragesz@gmail.com>
2023-09-28 23:56:36 -0400

    return new int[]{i, nMap.get(complement)};

---
+++ TwoSumRedo3.java
@@ -0,0 +1,19 @@
+// Two Sum
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumRedo3{
+	public static int[] twoSum(int[] nums, int target){
+		Map<Integer, Integer> nMap = new HashMap<>();
+		for(int i = 0; i < nums.length; i++){
+			nMap.put(nums[i], i);
+		}
+		for(int i = 0; i < nums.length; i++){
+			int complement = target - nums[i];
+			if(nMap.containsKey(complement) && nMap.get(complement) != i){
+				return new int[]{i, nMap.get(complement)};
+			}
+		}
+		return new int[]{};
+	}
+}
