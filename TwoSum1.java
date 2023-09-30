84733ab4bddc87cf0d56289e1d78fd66f24d94b5
MirageM <imiragesz@gmail.com>
2023-09-30 06:15:43 -0400

    nMap.containsKey(complement) containsKey containsKey

---
+++ TwoSum1.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public TwoSum1{
+	public static int[] twoSum(int[] nums, int target){
+		Map<Integer, Integer> nMap = new HashMap<>();
+		for(int i = 0; i < nums.length; i++){
+			nMap.put(nums[i], i);
+		}
+		for(int i = 0; i < nums.length; i++){
+			int complement = target - nums[i];
+			if(nMap.containsKey(complement) && nMap.get(complement)){
+				return new int[]{i, nMap.get(complement)};
+			}
+		}
+		return new int[]{};
+	}
+}
